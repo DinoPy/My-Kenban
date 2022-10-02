9edf1ae27a312d4c69f34f9a5dc06c746567f3f8
DinoPy <al3x.dinoiu@gmail.com>
2022-10-02 21:22:59 +0300

    removed example route

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,13 +1,11 @@
 import { taskRouter } from './task';
 // src/server/trpc/router/index.ts
 import { t } from '../trpc';
-import { exampleRouter } from './example';
 import { authRouter } from './auth';
 import { boardRouter } from './board';
 import { sectionRouter } from './section';
 
 export const appRouter = t.router({
-	example: exampleRouter,
 	auth: authRouter,
 	board: boardRouter,
 	section: sectionRouter,
